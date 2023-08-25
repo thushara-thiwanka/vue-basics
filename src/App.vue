b80ccd964454d2857573eb4d8375f40d11e6552c
Thushara Thiwanka <thushara.thiwanka@intrepidtravel.com>
2023-08-25 12:43:28 +0530

    feat: files added

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,53 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<img alt="Vue logo" src="./assets/logo.png" />
+	<HelloWorld msg="Welcome to Your Vue.js App" />
+	<teleport to=".modal">
+		<Modal
+			v-if="show_modal"
+			:title="title"
+			subtitle="Test Subtitle"
+			v-bind:button_text="button_text"
+			theme="dark"
+			@close_modal="toggle_modal"
+		>
+			<h1>Passed template with slot</h1>
+			<template v-slot:content>
+				<h2>Passed from named slot</h2>
+			</template>
+		</Modal>
+	</teleport>
+	<h2>{{ title }}</h2>
+	<button @click="toggle_modal">Show Modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		HelloWorld,
+		Modal,
+	},
+	data() {
+		return { title: "Test Title", button_text: "Button Text", show_modal: false };
+	},
+	methods: {
+		toggle_modal() {
+			this.show_modal = !this.show_modal;
+		},
+	},
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+	<div class="backdrop" v-on:click.self="close_modal">
+		<div class="content" v-bind:class="{ dark: theme === 'dark' }">
+			<slot></slot>
+			<slot name="content"></slot>
+			<h1 class="title">{{ title }}</h1>
+			<h2>{{ subtitle }}</h2>
+			<button>{{ button_text }}</button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "ModalComponent",
+	props: ["title", "subtitle", "button_text", "theme"],
+	methods: {
+		close_modal() {
+			this.$emit("close_modal");
+		},
+	},
+};
+</script>
+
+<style scoped>
+.backdrop {
+	width: 100%;
+	height: 100%;
+	position: fixed;
+	top: 0;
+	left: 0;
+	background-color: rgba(0, 0, 0, 0.25);
+	display: grid;
+	place-items: center;
+}
+.content {
+	width: 300px;
+	height: 200px;
+	background-color: white;
+	padding: 2rem;
+}
+.content.dark {
+	background-color: darkslategrey;
+	color: white;
+}
+</style>
